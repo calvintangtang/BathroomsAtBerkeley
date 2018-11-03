4646475f3d4e705427e2ef8522fd18158a197417
Calvin Tang <calvintang@berkeley.edu>
2018-11-03 10:44:39 -0700

    Adding proj files

---
+++ BathroomsAtBerkeley/BathroomsAtBerkeley/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BathroomsAtBerkeley
+//
+//  Created by Calvin Tang on 11/3/18.
+//  Copyright © 2018 Team :o. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BathroomsAtBerkeley/BathroomsAtBerkeley/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BathroomsAtBerkeley
+//
+//  Created by Calvin Tang on 11/3/18.
+//  Copyright © 2018 Team :o. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
