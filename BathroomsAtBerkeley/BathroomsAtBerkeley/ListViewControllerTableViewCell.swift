7d849779c409933be177cfd91cfce25d5d1d35f7
tsekylekobie <tse.kylekobie@berkeley.edu>
2018-11-04 02:33:48 -0800

    added controller file

---
+++ BathroomsAtBerkeley/BathroomsAtBerkeley/ListViewControllerTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ListViewControllerTableViewCell.swift
+//  BathroomsAtBerkeley
+//
+//  Created by Calvin Tang on 11/4/18.
+//  Copyright © 2018 Team :o. All rights reserved.
+//
+
+import UIKit
+
+class ListViewControllerTableViewCell: UITableViewCell {
+    @IBOutlet weak var bathroomName: UILabel!
+    
+    @IBOutlet weak var rating: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
